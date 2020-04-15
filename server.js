09f139c146052c42f33ab5cc3bbb8f834a3d694e
Peng Chen <pchen0816@outlook.com>
2020-04-15 12:26:09 -0700

    lab-07 finished

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 'use strict';
-
+// bring in the required library;
 require('dotenv').config();
 const express = require('express');
+const superagent = require('superagent');
+const cors = require('cors');
 const app = express();
-
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
-//location constructor
+
+
 class GetMap {
   constructor(city, data) {
     this.search_query = city;
@@ -16,57 +19,88 @@ class GetMap {
   }
 }
 
-class GetWeather {
-  constructor(summary, time){
-    this.forecast = summary;
-    this.time = new Date(time).toString().slice(0,15);
-  }
+
+function getLocation (request, response) {
+  const cityName = request.query.city;
+  const locationKey = process.env.GEOCODE_API_KEY;
+  const locationUrl = `https://us1.locationiq.com/v1/search.php?key=${locationKey}&q=${cityName}&format=json&limit=1`
+  superagent.get(locationUrl)
+    .then(locationResponse => {
+      const cityData = locationResponse.body;
+      for (let i in cityData){
+        if (cityData[i].display_name.toLowerCase().includes(cityName)){
+          let location = new GetMap(cityName,cityData[i]);
+          response.status(200).send(location);
+        }
+      }
+    })
+    .catch(error => {
+      errorHandler("Can't find city name", request, response);
+    })
 }
 
-// bring in our library;
-const cors = require('cors');
-// plug our library into express
-app.use(cors()); // configures our cross origing resource sharing.
-
-var error = {
-  status: 500,
-  responseText: "Sorry, something went wrong",
-};
-
-var weatherChecker;
-app.get('/location', (request, response) => {
-  weatherChecker = false;
-  let cityName = request.query.city;
-  let data = require('./data/geo.json');
-  let location = error;
-  for (let i in data){
-    if (data[i].display_name.toLowerCase().includes(cityName)){
-      location = new GetMap(cityName,data[i]);
-      weatherChecker = true;
-      break;
-    }
-  }
-  response.status(200).send(location);
-});
-
-app.get('/weather', (request, response) => {
-  if (weatherChecker === true){
-    let weatherArr = [];
-    let weatherData = require('./data/weather.json').data;
-    for (let i in weatherData){
-      let summary = weatherData[i].weather.description;
-      let time = weatherData[i].datetime;
-      weatherArr.push(new GetWeather(summary, time));
-    }
-    response.status(200).send(weatherArr);
-  } else {
-    response.send(error);
-  }
-});
-app.use(errorHandler);
-function errorHandler(request, response) {
-  response.status(500).send('Sorry, something went wrong');
+function getWeather (request, response) {
+  const weatherLat = request.query.latitude;
+  const weatherLon = request.query.longitude;
+  const weatherKey = process.env.WEATHER_API_KEY;
+  const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${weatherLat}&lon=${weatherLon}&key=${weatherKey}`;
+  superagent.get(weatherUrl)
+    .then(weatherResponse =>{
+      const weatherData = weatherResponse.body.data;
+      let weatherResult = weatherData.map(item => {
+        return {
+          forecast : item.weather.description,
+          time : new Date(item.datetime).toString().slice(0,15),
+        }
+      });
+      response.status(200).send(weatherResult);
+    })
+    .catch(error => {
+      errorHandler("Can't find weather data for that location", request, response);
+    })
 }
 
+function getTrails(request,response){
+  const trailsLat = request.query.latitude;
+  const trailsLon = request.query.longitude;
+  const trailsKey = process.env.TRAIL_API_KEY;
+  const trailsUrl = `https://www.hikingproject.com/data/get-trails?lat=${trailsLat}&lon=${trailsLon}&maxDistance=10&key=${trailsKey}`;
+  superagent.get(trailsUrl)
+    .then(trailsResponse => {
+      const trailsData = trailsResponse.body.trails;
+      let trailsResult = trailsData.map(item => {
+        return {
+          name : item.name,
+          location : item.location,
+          length : item.length,
+          stars : item.stars,
+          star_votes : item.starVotes,
+          summary : item.summary,
+          trail_url : item.url,
+          conditions : item.conditionDetails,
+          condition_date : item.conditionDate.slice(0,11),
+          condition_time : item.conditionDate.slice(11,20),
+        }
+      })
+      response.status(200).send(trailsResult);
+    })
+    .catch(error => {
+      errorHandler("Can't find trail data for that location", request, response);
+    })
+
+}
+function errorHandler(error, request, response) {
+  console.log(error);
+
+  response.status(500).send(
+    {
+      status: 500,
+      responseText: 'Sorry, something went wrong'
+    });
+}
+
+app.get('/location', getLocation);
+app.get('/weather', getWeather);
+app.get('/trails', getTrails);
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
 
